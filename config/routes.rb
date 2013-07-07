495a34b07d4813e305a4e90df3a2156859255f4c
eslafroggy <claire.pierce@gmail.com>
2013-07-07 12:20:52 -0700

    added view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Fandangr::Application.routes.draw do
-  get "showtimes/show"
+  resources :movies, only: [:index, :show] do
+    resources :showtimes, only: :show do
+      post :purchase
+    end
+  end
 
-  resources :movies, only: [:index, :show]
   root to: "movies#index"
-end 
+end
 

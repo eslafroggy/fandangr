93f4fe320f266baf37f6b529c4aa6ae3b68828d9
eslafroggy <claire.pierce@gmail.com>
2013-07-07 12:27:27 -0700

    added javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,27 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(function () {
+  $("td").on("click", function (event) {
+    var table, movieID, showtimeID, seat, seatRow, seatNumber;
+
+    event.preventDefault();
+
+    seat = $(this);
+    seatRow = seat.data("row");
+    seatNumber = seat.data("seat-number");
+
+    table = seat.closest("table");
+    movieID = table.data("movie-id");
+    showtimeID = table.data("showtime-id");
+
+    jqXHR = $.post("/movies/" + movieID + "/showtimes/" + showtimeID + "/purchase", {
+      row: seatRow,
+      "seat_number": seatNumber
+    }).done(function () {
+      seat.attr("class", "error").text("Purchased");
+    }).fail(function () {
+      alert("Seat cannot be purchased.");
+    });
+  });
+});
